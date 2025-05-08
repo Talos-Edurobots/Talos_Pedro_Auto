30e6af7e634dd92ae0bdd574373f7a3babd696f5
ManosFps1pt <manos.dragasakis@gmail.com>
2025-05-08 13:02:30 +0300

    update offset from cad

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -13,7 +13,7 @@ public class LConstants {
         OTOSConstants.hardwareMapName = "otos";
         OTOSConstants.linearUnit = DistanceUnit.INCH;
         OTOSConstants.angleUnit = AngleUnit.RADIANS;
-        OTOSConstants.offset = new SparkFunOTOS.Pose2D(2.71, 0, - Math.PI / 2);//Math.PI / 2///
+        OTOSConstants.offset = new SparkFunOTOS.Pose2D(2.851732283464567, 0, - Math.PI / 2);//Math.PI / 2///
         OTOSConstants.linearScalar = 0.99345;
         OTOSConstants.angularScalar = 0.9745;
     }
